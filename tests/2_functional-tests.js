6142fcbd12d382eefe24f66a86c32272dd185056
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2024-07-12 09:32:58 +0300

    add assert function

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,21 +6,21 @@ const server = require('../server');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-// suite('Functional Tests', function () {
-//   this.timeout(5000);
-//   suite('Integration tests with chai-http', function () {
+suite('Functional Tests', function () {
+  this.timeout(5000);
+  suite('Integration tests with chai-http', function () {
 //     // #1
-//     test('Test GET /hello with no name', function (done) {
-//       chai
-//         .request(server)
-//         .keepOpen()
-//         .get('/hello')
-//         .end(function (err, res) {
-//           assert.fail(res.status, 200);
-//           assert.fail(res.text, 'hello Guest');
-//           done();
-//         });
-//     });
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done();
+        });
+    });
 //     // #2
 //     test('Test GET /hello with your name', function (done) {
 //       chai
@@ -81,5 +81,5 @@ chai.use(chaiHttp);
 
 //       done();
 //     });
-//   });
-// });
+  });
+});
